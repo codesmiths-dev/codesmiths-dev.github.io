fc5688f84d177807f04e539525e50ec69a956860
cstayyab <cstayyab@gmail.com>
2020-04-19 19:23:25 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Button } from 'reactstrap'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Website Coming Soon</h1>
+        <Button color="success" href="https://fiverr.com/m5_innovations">Hire M5 Innovations</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
